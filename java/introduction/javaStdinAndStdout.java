00af4b9cf85227a8babcd08c0eea4f08251258af
_pravakar <61041599+bigOcodes@users.noreply.github.com>
2021-11-04 15:29:15 +0530

    Create javaStdinAndStdout.java

---
+++ java/introduction/javaStdinAndStdout.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int myInt1 = scanner.nextInt();
+        int myInt2 = scanner.nextInt();
+        int myInt3 = scanner.nextInt();
+        scanner.close();
+        
+        System.out.println(myInt1+"\n"+myInt2+"\n"+myInt3);
+    }
+}
